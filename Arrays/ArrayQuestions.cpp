fb56ec9841beeda1ffe1a1521c36e5a00946619b
dibkb <dibas9110@gmail.con>
2022-09-12 15:17:23 +0530

    Swap alternatives in array

--- Arrays/ArrayQuestions.cpp
+++ Arrays/ArrayQuestions.cpp
@@ -20,15 +20,28 @@ void printArray(int array[],int size){
       cout << array[i] <<" ";
    };
 }
+void swapAlternates(int array[],int size){
+   for (int i= 0; i< size -1;i++){
+      if(i %2 == 0){
+         swap(array[i],array[i+1]);
+      }
+   }
+}
 int main(){
    // reversing an array;
    int myArrayOne[6] = {1,2,3,4,5,6};
    int myArrayTwo[5] = {48,23,1,23,-4};
    // ----- reverse function-------------
-   reverseArray(myArrayOne,6);
-   reverseArray(myArrayTwo,5);
-   cout <<"PRINTING REVERSED ARRAY " <<endl;
-   printArray(myArrayOne,6);
+   // reverseArray(myArrayOne,6);
+   // reverseArray(myArrayTwo,5);
+   // cout <<"PRINTING REVERSED ARRAY " <<endl;
+   // printArray(myArrayOne,6);
+   // cout<<endl;
+   // printArray(myArrayTwo,5);
+   // --------------SWAP ALTERNATES-------------
+   swapAlternates(myArrayOne,6);
+   swapAlternates(myArrayTwo,5);
+      printArray(myArrayOne,6);
    cout<<endl;
    printArray(myArrayTwo,5);
    return 0;
